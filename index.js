276b18cdfdfadbb3ef45154d0a0556eb570663d1
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-05-22 12:49:17 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code here
+class Breakfast{
+    constructor(food,drink){
+        this.food = food
+        this.drink = drink
+    }
+}
+
+let todayB = new Breakfast("anjera","Tea")
+
+class Lunch{
+    constructor(salad,soup,drink){
+        this.salad = salad
+        this.soup = soup
+        this.drink = drink
+    }
+}
+
+let todayL = new Lunch('salito','bone soup','passion')
+
+class Dinner{
+    constructor(salad,soup,entree,dessert){
+        this.salad = salad
+        this.soup = soup
+        this.entree = entree
+        this._dessert = dessert
+    }
+}
+
+let todayD = new Dinner("salu",'bone soup','cholate cake','cheese')
+
+
